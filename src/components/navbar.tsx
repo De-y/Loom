0c351ce4066758451b8367965abdfd894929ed32
Defy <61808223+De-y@users.noreply.github.com>
2024-01-28 10:42:53 -0800

    e

---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+import '@/css/components/navbar.css'
+export default function Navbar() {
+    return (
+        <div className='navbar'>
+            <div  className='logo'>
+                <img src='/loom.png'/>
+                <h1>Loom</h1>
+            </div>
+        </div>
+    )
+}
