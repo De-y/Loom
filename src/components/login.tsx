434b97893a4128f4b344744a0c0d1dfc2244bf89
Defy <61808223+De-y@users.noreply.github.com>
2024-01-30 10:18:04 -0800

    bru

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -2,11 +2,15 @@
 
 import '@/css/login.css'
 export default function LoginIt() {
+    var message = ''
     const submit = function() {
         const username = document.getElementById('username').value
         const password = document.getElementById('password').value
         if (password && username) {
-            console.log('K')
+            
+        } else {
+            document.getElementById('errsucc').style.display =  'block'
+            document.getElementById('err-message').innerHTML = "No Username or Password Provided."
         }
     }
     return (
@@ -19,6 +23,12 @@ export default function LoginIt() {
                 </div>
                 <p>Welcome back! Log in with your account.</p>    
                 <br />
+                <div className='errsucc' id='errsucc'>
+                    <div className='error_success'>
+                        <p id='err-message'>{message}e</p>
+                    </div>
+                    <br />
+                </div>
                 <div className='form'>
                     <input className='input' type='text' placeholder='Username' name="Username" id="username"/>
                     <input className='input' type='password' placeholder='Password' name="Password" id="password"/>
