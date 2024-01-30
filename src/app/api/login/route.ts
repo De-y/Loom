3a49515a3a4dcd0f70504ed71bd7b464c18feae1
Defy <61808223+De-y@users.noreply.github.com>
2024-01-30 12:45:35 -0800

    authy authy

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,9 +1,21 @@
 import { NextResponse } from "next/server"
 import * as jwt from 'jose'
+import { createSecretKey } from "crypto";
 
 export async function POST(request: Request) {
   const data = await request.json();
   const { username, password } = data;
+  let token, authenticated;
+  if (username == 'admin' && password=='admin') {
+    token = await new jwt.SignJWT({id: username}).setProtectedHeader({alg: 'HS256'}).setAudience('Loom').setExpirationTime('1 year').sign(createSecretKey(process.env.JWT_Secret, 'utf-8'))
+    authenticated = true
+  } else {
+    token = undefined;
+    authenticated = false;
+  }
   console.log(username, password)
-  return NextResponse.json({ message: "Deez nuts" }, { status: 200 });
+  return NextResponse.json({
+    'token': token,
+    'authenticated': authenticated,
+  })
 }
--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,20 +1,27 @@
 'use client'
 
 import '@/css/login.css'
-import { fetch } from "popsicle";
+import { setCookie } from 'cookies-next'
 export default function LoginIt() {
     var message = ''
-    const submit = function() {
+    const submit = async function() {
         const username = document.getElementById('username').value
         const password = document.getElementById('password').value
         if (password && username) {
-            fetch('/api/login', {
+            const responseData = await fetch('/api/login', {
                 method: 'POST',
                 body: JSON.stringify({
                     username: username,
                     password: password
                 }),
             })
+            const {token, authenticated} = await responseData.json()
+            if (authenticated != true && token == undefined) {
+                document.getElementById('errsucc').style.display =  'block'
+                document.getElementById('err-message').innerHTML = "Incorrect username or password."                    
+            } else {
+                setCookie('authentication', token, {'secure': true, 'sameSite': 'strict'})
+            }
         } else {
             document.getElementById('errsucc').style.display =  'block'
             document.getElementById('err-message').innerHTML = "No Username or Password Provided."
