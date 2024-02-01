6bca25b87851bde7b6f392772ed86800d2bf89c1
Defy <61808223+De-y@users.noreply.github.com>
2024-02-01 08:53:05 -0800

    hmm

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -1,46 +1,46 @@
-import { NextResponse } from "next/server";
-import * as jwt from 'jose'
-import { createSecretKey } from "crypto";
-import db from '@/db/prisma'
-import { parse } from 'cookie'
+import * as jwt from 'jose';
+import { createSecretKey } from 'crypto';
+import db from '@/db/prisma';
+import { parse } from 'cookie';
 
 export async function GET(request: Request) {
     try {
         // @ts-ignore
-        let token = request.headers.get('cookie')
+        let token = request.headers.get('cookie');
 
         if (!token) {
-            return NextResponse.json({
-                'authenticated': false,
-                'authID': 'MISSING_TOKEN',
-            });
+            return {
+                authenticated: false,
+                authID: 'MISSING_TOKEN',
+            };
         }
         // @ts-ignore
-        let decision = await jwt.jwtVerify(parse(token)['authorization'], createSecretKey(process.env.jwt_secret, 'utf-8'))
+        let decision = await jwt.jwtVerify(parse(token)['authorization'], createSecretKey(process.env.jwt_secret, 'utf-8'));
         let accountLookupService = await db.user.findFirst({
             where: {
                 // @ts-ignore
-                'email': decision?.payload.id
-            }
-        })
+                email: decision?.payload.id,
+            },
+        });
 
         // @ts-ignore
-        if (decision.payload.aud === 'Loom' && accountLookupService != null && decision?.payload.exp * 1000 >= new Date().getTime()) {
-            return NextResponse.json({
-                'authenticated': true,
-            })
+        if (decision.payload.aud === 'Loom' && accountLookupService != null && decision?.payload.exp * 1000 >= new Date().getTime()
+        ) {
+            return {
+                authenticated: true,
+            };
         }
 
-        return NextResponse.json({
-            'authenticated': false,
-            'authID': 'VEJ',
-            'debugAuth': [token, decision, accountLookupService]
-        })
+        return {
+            authenticated: false,
+            authID: 'VEJ',
+            debugAuth: [token, decision, accountLookupService],
+        };
     } catch (err) {
         console.log(err);
-        return NextResponse.json({
-            'authenticated': false,
-            'authID': 'SEI',
-        })
+        return {
+            authenticated: false,
+            authID: 'SEI',
+        };
     }
 }
