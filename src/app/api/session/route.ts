79b8443776b56ed6e59faf979e0cb458d56e29a3
Defy <61808223+De-y@users.noreply.github.com>
2024-02-01 08:56:56 -0800

    hmm

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -1,46 +1,37 @@
-import * as jwt from 'jose';
-import { createSecretKey } from 'crypto';
-import db from '@/db/prisma';
-import { parse } from 'cookie';
+import { NextRequest, NextResponse } from "next/server";
+import * as jwt from 'jose'
+import { createSecretKey } from "crypto";
+import db from '@/db/prisma'
+import {parse} from 'cookie'
 
-export async function GET(request: Request) {
+export default async function handler(req: NextRequest, res: NextResponse) {
     try {
         // @ts-ignore
-        let token = request.headers.get('cookie');
-
-        if (!token) {
-            return {
-                authenticated: false,
-                authID: 'MISSING_TOKEN',
-            };
-        }
+        let token = request.headers.get('cookie')
         // @ts-ignore
-        let decision = await jwt.jwtVerify(parse(token)['authorization'], createSecretKey(process.env.jwt_secret, 'utf-8'));
+        let decision = await jwt.jwtVerify(parse(token)['authorization'], createSecretKey(process.env.jwt_secret, 'utf-8'))
         let accountLookupService = await db.user.findFirst({
             where: {
                 // @ts-ignore
-                email: decision?.payload.id,
-            },
-        });
-
+                'email': decision?.payload.id
+            }
+        })
         // @ts-ignore
-        if (decision.payload.aud === 'Loom' && accountLookupService != null && decision?.payload.exp * 1000 >= new Date().getTime()
-        ) {
+        if (decision.payload.aud == 'Loom' && accountLookupService != null && decision?.payload.exp * 1000 >= new Date().getTime()) {
             return {
-                authenticated: true,
-            };
+                'authenticated': true,
+            }
         }
-
         return {
-            authenticated: false,
-            authID: 'VEJ',
-            debugAuth: [token, decision, accountLookupService],
-        };
+            'authenticated': false,
+            'authID': 'VEJ',
+            'debugAuth': [token, decision, accountLookupService]
+        }
     } catch (err) {
-        console.log(err);
+        console.log(err)
         return {
-            authenticated: false,
-            authID: 'SEI',
-        };
+            'authenticated': false,
+            'authID': 'SEI',
+        }
     }
 }
