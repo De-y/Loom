230d64925dee6c173d92d49111dc8828de68d301
Defy <61808223+De-y@users.noreply.github.com>
2024-02-09 19:53:00 -0800

    aux fix

--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -7,26 +7,20 @@ import { useEffect } from 'react';
 import { CircularProgress } from '@mui/material';
 
 export default function LogoutIt() {   
-    try {
-        deleteCookie('authorization')
-        useEffect(() => {
-            redirect('/login')
-        })
-        return (
-            <>
-                <div className='loggingout'>
-                    <h1>Loom is logging you out.</h1>
-                    <br />
-                    <div className='loader'>
-                        <CircularProgress className='loom-load'/>                    
-                    </div>
+    deleteCookie('authorization')
+    useEffect(() => {
+        redirect('/login')
+    })
+
+    return (
+        <>
+            <div className='loggingout'>
+                <h1>Loom is logging you out.</h1>
+                <br />
+                <div className='loader'>
+                    <CircularProgress className='loom-load'/>                    
                 </div>
-            </>
-        )
-    } catch (err) {
-        console.log(err)
-        return (
-            deleteCookie('authorization')
-        )
-    }
+            </div>
+        </>
+    )
 }
