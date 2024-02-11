5663b22bb81a6985ffe9268651ea9dfc7f272329
Defy <61808223+De-y@users.noreply.github.com>
2024-02-10 20:28:42 -0800

    dynafix

--- src/components/admin.tsx
+++ src/components/admin.tsx
@@ -28,6 +28,26 @@ export default function AdminApprove() {
             })
         })
     }
+    function userALL() {
+        fetch('/api/admin/approve', {
+            'method': 'POST',
+            'body': JSON.stringify({
+                'token': getCookie('authorization'),
+                'confirm': {
+                    'all': true,
+                }
+            })
+        }).then((id) => {
+            id.json().then((ok) => {
+                if (ok.status == 'ok') {
+                    e.push('/admin')
+                } else {
+                    e.push('/admin')
+                }
+            })
+        })
+    }
+
     return (
         <>
                 <form action={userCONFIRM}>
@@ -35,7 +55,7 @@ export default function AdminApprove() {
                     <input type="text" placeholder="User ID" id="UserID"/>
                     <button type="submit" id="submit">Submit</button>
                 </form>
-                <form action="#">
+                <form action={userALL}>
                     <h1>Approve All</h1>
                     <button type="submit" id="submit">Submit</button>
                 </form>
