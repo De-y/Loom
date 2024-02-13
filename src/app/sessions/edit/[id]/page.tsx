c7333ed8ab088b4645e2a0b0daddbd4cb1c7ab52
Defy <61808223+De-y@users.noreply.github.com>
2024-02-13 10:18:20 -0800

    mr

--- src/app/sessions/edit/[id]/page.tsx
+++ src/app/sessions/edit/[id]/page.tsx
@@ -1,7 +1,16 @@
+import '@/css/sessions/edit.css'
+import db from "@/db/prisma"
+import Image from 'next/image'
 export default async function editSessionInformation({ params }: { params: { id: string } }) {
     return (
         <>
-            <h1>DONE</h1>
+            <div className='navbar'>
+                <a className='logo' href="/dashboard">
+                    <Image width={50} height={50} alt='Loom Logo' src={'/loom.png'} />
+                    <span>Loom</span>
+                </a>
+            </div>
+            <h1></h1>
         </>
     )
 };
