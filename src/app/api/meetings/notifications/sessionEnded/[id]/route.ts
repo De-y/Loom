e140734d63a90c5f1cd0166a19bb09c679f8426a
Defy <61808223+De-y@users.noreply.github.com>
2024-02-16 11:35:37 -0800

    tests

--- src/app/api/meetings/notifications/sessionEnded/[id]/route.ts
+++ src/app/api/meetings/notifications/sessionEnded/[id]/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server"
 export async function GET(request: Request) {
     let i = request.url.split('/')
     // @ts-ignore
-
+    console.log(request.headers)
     i = i[i.length - 1]
     console.log(i)
     return NextResponse.json({
